25fc9a348e3fe68560830d2e91bdfabf63c7e2fa
James Gallagher <james@Opencommit.com>
2020-06-08 14:35:24 +0100

    feat: Fix app

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -27,7 +27,7 @@ class GraphqlController < ApplicationController
       return nil if request.headers['Authorization'].blank?
       token = request.headers['Authorization'].split(' ').last
       return nil if token.blank?
-      AuthToken.verify(token)
+      JWT.verify(token)
     else
       return 0
     end
--- app/models/auth_token.rb
+++ app/models/auth_token.rb
@@ -2,12 +2,10 @@ class AuthToken
   def self.key
     Rails.application.secrets.secret_key_base
   end
-
   def self.token(user)
     payload = {user_id: user.id}
     JsonWebToken.sign(payload, key: key)
   end
-
   def self.verify(token)
     result = JsonWebToken.verify(token, key: key)
     return nil if result[:error]
